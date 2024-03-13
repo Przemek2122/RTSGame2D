6872cbd7017d32c49ce02ebdb9ad5ab7fb5d1540
Przemek <przemek12wiewiora@gmail.com>
2024-03-13 21:04:33 +0100

    Add Component to unit factory

--- Source/Private/ECS/UnitFactoryBase.cpp
+++ Source/Private/ECS/UnitFactoryBase.cpp
@@ -1,2 +1,17 @@
 #include "GamePCH.h"
 #include "ECS/UnitFactoryBase.h"
+
+#include "ECS/Components/TransformComponent.h"
+
+EUnitFactoryBase::EUnitFactoryBase(FEntityManager* InEntityManager)
+	: EEntity(InEntityManager)
+{
+	CreateComponent<UTransformComponent>("TransformComponent");
+
+
+
+}
+
+EUnitFactoryBase::~EUnitFactoryBase()
+{
+}
--- Source/Public/ECS/UnitFactoryBase.h
+++ Source/Public/ECS/UnitFactoryBase.h
@@ -1,7 +1,13 @@
 #pragma once
 
-#include "Core\ECS\Entity.h"
+#include "Core/ECS/Entity.h"
 
 class EUnitFactoryBase : public EEntity
 {
+public:
+	EUnitFactoryBase(FEntityManager* InEntityManager);
+	virtual ~EUnitFactoryBase();
+
+
+
 };
