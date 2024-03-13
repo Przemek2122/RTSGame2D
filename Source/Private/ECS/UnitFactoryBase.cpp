c4c5e2c56053cd2aa9441b0d31584b29d6d6dc1b
Przemek <przemek12wiewiora@gmail.com>
2024-03-13 22:21:30 +0100

    Add second component

--- Source/Private/ECS/UnitFactoryBase.cpp
+++ Source/Private/ECS/UnitFactoryBase.cpp
@@ -1,12 +1,14 @@
 #include "GamePCH.h"
 #include "ECS/UnitFactoryBase.h"
 
+#include "ECS/Components/RenderComponent.h"
 #include "ECS/Components/TransformComponent.h"
 
 EUnitFactoryBase::EUnitFactoryBase(FEntityManager* InEntityManager)
 	: EEntity(InEntityManager)
 {
 	CreateComponent<UTransformComponent>("TransformComponent");
+	CreateComponent<URenderComponent>("RenderComponent");
 
 
 
