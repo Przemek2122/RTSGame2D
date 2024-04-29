43738a1cdd6f536d719621fea3f505bd672d2b06
Przemek <przemek12wiewiora@gmail.com>
2024-04-29 12:11:11 +0200

    Update RTSAssetCollection.h

--- Source/Public/Core/RTSAssetCollection.h
+++ Source/Public/Core/RTSAssetCollection.h
@@ -8,8 +8,8 @@
 class RTSAssetCollection : public FBaseAssetsCollection
 {
 public:
-	inline static FAssetCollectionItem FactoryBase = { "FactoryBase", R"(Assets\Textures\Factories\FactoryBase.png)" };
-	inline static FAssetCollectionItem UnitBase = { "Unit", R"(Assets\Textures\Units\UnitBase.png)" };
+	inline static const FAssetCollectionItem FactoryBase = { "FactoryBase", R"(Assets\Textures\Factories\FactoryBase.png)" };
+	inline static const FAssetCollectionItem UnitBase = { "Unit", R"(Assets\Textures\Units\UnitBase.png)" };
 
 	// Add more assets here
 	// ...
